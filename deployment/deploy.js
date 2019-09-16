d3f32ea002f94dd900e440be9a87f3490e71a36d
Anton Sotirov <anton.k.sotirov@gmail.com>
2019-09-16 15:50:51 +0200

    Added gir-ocppjs charge point simulator. Added .gitignore. Added WattContract.aes basic registry.

--- deployment/deploy.js
+++ deployment/deploy.js
@@ -19,7 +19,7 @@ const Deployer = require('aeproject-lib').Deployer;
 const deploy = async (network, privateKey, compiler, networkId) => {
     let deployer = new Deployer(network, privateKey, compiler, networkId)
 
-    await deployer.deploy("./contracts/ExampleContract.aes")
+    await deployer.deploy("./contracts/WattContract.aes")
 };
 
 module.exports = {
